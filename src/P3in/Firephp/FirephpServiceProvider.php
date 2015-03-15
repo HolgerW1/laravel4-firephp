f05e8a274f46039c7d23ec00d0af75883862b2d7
Holger Wiedemann <holger.wiedemann@wirsind.info>
2015-03-15 02:25:30 +0100

    Migrated to support Laravel 5

--- src/P3in/Firephp/FirephpServiceProvider.php
+++ src/P3in/Firephp/FirephpServiceProvider.php
@@ -1,101 +1,138 @@
-<?php namespace P3in\Firephp;
+<?php
+
+namespace P3in\Firephp;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Events\Dispatcher;
 use P3in\Firephp\FirePHPCore\FirePHP;
 use P3in\Firephp\FirePHPCore\FB;
 use App;
 use Config;
 
-class FirephpServiceProvider extends ServiceProvider {
+class FirephpServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
-	var $pack = 'p3in/firephp';
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package($this->pack);
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @param  Dispatcher $events
+     * @return void
+     */
+    public function boot(Dispatcher $events)
+    {
+        $this->setupConfig();
+        $this->registerEventListeners($events);
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['config']->package($this->pack, __DIR__.'/../../config');
+    /**
+     * Setup the config.
+     *
+     * @return void
+     */
+    protected function setupConfig()
+    {
+        $source = realpath(__DIR__ . '/../../../config/firephp.php');
+        $this->publishes([$source => config_path('firephp.php')]);
+        $this->mergeConfigFrom($source, 'firephp');
+    }
 
-		$this->app['fb'] = $this->app->share(function($app){
-			$fb = new FB;
-			$fb->setEnabled(Config::get('firephp::enabled'));
-			return $fb;
-		});
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerFirephp();
+    }
 
-		$this->app->booting(function()
-		{
-			$loader = \Illuminate\Foundation\AliasLoader::getInstance();
-			$loader->alias('FB', 'P3in\Firephp\Facades\FB');
-		});
-		if(!App::runningInConsole()){
-			$this->app->events->listen('illuminate.log', function($level, $message, $context){
-				$type = 'log';
-				$errorInfoKeys = Config::get('firephp::error_keys_and_compact_check');
-				switch (strtoupper($level)){
-					case FirePHP::INFO :
-					case FirePHP::WARN :
-					case FirePHP::LOG :
-					case FirePHP::ERROR :
-						$type = $level;
-					break;
-					case 'WARNING':
-						$type = 'warn';
-					break;
-				}
-				if (is_object($message)) {
-					$groupLabel = $message->getMessage() ? $message->getMessage() : 'Error';
-					$this->app['fb']->group($groupLabel);
-						foreach ($errorInfoKeys as $key => $compact_check) {
-							if ($data = $message->{$key}()) {
-								if ($compact_check) {
-									$data = json_decode(json_encode($data));
-								}
-								$this->app['fb']->{$type}($data,substr($key,3,strlen($key)));
-							}
-						}
-					$this->app['fb']->groupEnd();
-				}else{
-					$this->app['fb']->{$type}($message);
-				}
-			});
-			if (Config::get('firephp::db_profiler')) {
-				$this->app->events->listen('illuminate.query', function($query, $params, $time, $conn){
-					$this->app['fb']->group("$conn Query");
-						$this->app['fb']->log($query,'Query String');
-						$this->app['fb']->log($params,'Parameters');
-						$this->app['fb']->log($time,'Execution Time');
-					$this->app['fb']->groupEnd();
-				});
-			}
-		}
-	}
+    /**
+     * 
+     */
+    protected function registerFirephp()
+    {
+        $this->app->singleton('firephp', function($app) {
+            $config = $app['config'];
+            $fb = new FB;
+            $fb->setEnabled($config['firephp.enabled']);
+            return $fb;
+        });
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('firephp');
-	}
+        $this->app->alias(
+                'firephp', 'P3in\Firephp\Facades\FB'
+        );
+    }
+
+    /**
+     * Register the event listeners.
+     *
+     * @param  Dispatcher $events
+     * @return void
+     */
+    protected function registerEventListeners(Dispatcher $events)
+    {
+        if (!App::runningInConsole()) {
+            $config = $this->app['config'];
+            
+            $events->listen('illuminate.log', function($level, $message, $context) {
+                $type = 'log';
+                $errorInfoKeys = Config::get('firephp::error_keys_and_compact_check');
+                switch (strtoupper($level))
+                {
+                    case FirePHP::INFO :
+                    case FirePHP::WARN :
+                    case FirePHP::LOG :
+                    case FirePHP::ERROR :
+                        $type = $level;
+                        break;
+                    case 'WARNING':
+                        $type = 'warn';
+                        break;
+                }
+                if (is_object($message)) {
+                    $groupLabel = $message->getMessage() ? $message->getMessage() : 'Error';
+                    $this->app['firephp']->group($groupLabel);
+                    foreach ($errorInfoKeys as $key => $compact_check) {
+                        if ($data = $message->{$key}()) {
+                            if ($compact_check) {
+                                $data = json_decode(json_encode($data));
+                            }
+                            $this->app['firephp']->{$type}($data, substr($key, 3, strlen($key)));
+                        }
+                    }
+                    $this->app['firephp']->groupEnd();
+                } else {
+                    $this->app['firephp']->{$type}($message);
+                }
+            });
+
+            if ($config['firephp.db_profiler']) {
+                $events->listen('illuminate.query', function($query, $params, $time, $conn) {
+                    $firephp = $this->app['firephp'];
+                    $firephp->group("$conn Query");
+                    $firephp->log($query, 'Query String');
+                    $firephp->log($params, 'Parameters');
+                    $firephp->log($time, 'Execution Time');
+                    $firephp->groupEnd();
+                });
+            }
+        }
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('firephp');
+    }
 
 }
